9ae999b9165e636f37a7c554ce02aaa761648831
Leila <nb.leila10@gmail.com>
2019-04-16 16:17:38 -0500

    Add test to be sure Display renders without crashing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,12 +1,19 @@
 import React from "react"
 import renderer from "react-test-renderer"
+import { render, cleanup } from "react-testing-library"
 
 import Display from "./Display"
+
+afterEach(cleanup)
 
 describe("<Display />", () => {
   it("matches snapshot", () => {
     const tree = renderer.create(<Display />)
 
     expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  it("renders without crashing", () => {
+    render(<Display />)
   })
 })
