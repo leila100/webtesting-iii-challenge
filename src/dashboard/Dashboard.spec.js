d989cb3b732653d2e05fda9843a3266c2081b600
Leila <nb.leila10@gmail.com>
2019-04-16 16:14:55 -0500

    Add test to be sure Dashboard renders without crashing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,12 +1,19 @@
 import React from "react"
 import renderer from "react-test-renderer"
+import { render, cleanup } from "react-testing-library"
 
 import Dashboard from "./Dashboard"
+
+afterEach(cleanup)
 
 describe("<Dashboard />", () => {
   it("matches snapshot", () => {
     const tree = renderer.create(<Dashboard />)
 
     expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  it("renders without crashing", () => {
+    render(<Dashboard />)
   })
 })
