b80c69c19fdcefdc949dcda4f080d67638cbd1a3
Leila <nb.leila10@gmail.com>
2019-04-16 16:06:06 -0500

    Add snapshot of Display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,12 @@
-// Test away!
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Display from "./Display"
+
+describe("<Display />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Display />)
+
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+})
