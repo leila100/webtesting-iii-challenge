b531da5324920648003c0677a82546b75cef7ea9
Leila <nb.leila10@gmail.com>
2019-04-16 16:16:22 -0500

    Add test to be sure Controls renders without crashing

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,12 +1,19 @@
 import React from "react"
 import renderer from "react-test-renderer"
+import { render, cleanup } from "react-testing-library"
 
 import Controls from "./Controls"
+
+afterEach(cleanup)
 
 describe("<Controls />", () => {
   it("matches snapshot", () => {
     const tree = renderer.create(<Controls />)
 
     expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  it("renders without crashing", () => {
+    render(<Controls />)
   })
 })
